eb9f1e6d284501a14b96e31f8bb4765d392067ac
Ghazal Minaei <ghazal@Ghazals-MacBook-Air-2.local>
2023-03-09 23:50:13 -0800

    Fix: fix static

--- Financial/settings.py
+++ Financial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bi*elv&ramm2c0%li64aa59fn(xs5k!i&w+7g02ffh+^!k4z65'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
@@ -127,11 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = 'static'
-
 STATIC_URL = '/statics/'
-
-STATICFILES_DIRS = ('statics',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
