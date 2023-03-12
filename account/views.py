9db4241be5a201b4ddfa3ac37cc4bf0f47667db0
Nima Modares Gorji <nima.mg@gmail.com>
2023-03-12 01:31:28 -0800

    Complete Account Viewset

--- account/views.py
+++ account/views.py
@@ -1,35 +1,53 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import F
+import os
 
+import requests
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from drf_yasg.utils import swagger_auto_schema
+from dotenv import load_dotenv
 
 from account.models import Account
-from account.serializers import AccountSerializer
+from account.serializers import AccountSerializer, PaySerializer
 from invoice.serializers import InvoiceSerializer
+
+load_dotenv()
+
+GET_USER_ID_URL = 'http://{}/api/users/{}/'.format(os.environ.get('ACCOUNTS_ROOT_URL'), '{}')
 
 
 class AccountViewset(ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-# order_id, user_id, a list of price, quantity, product
 
+    # Get user_id from accounts service
+    def get_object(self):
+        id = self.kwargs.get('pk')
+        if not id.isnumeric():
+            data = requests.get(GET_USER_ID_URL.format(id)).json()
+            return Account.objects.get(user_id=data.get('id'))
+        else:
+            return super().get_object()
+
+    @swagger_auto_schema(method='post', responses={200: InvoiceSerializer}, request_body=PaySerializer)
     @action(detail=True, methods=['post'])
     def pay(self, request, pk=None):
-        user_id = request.data.pop('user_id')
+        serializer = PaySerializer(data=request.data)
+        if not serializer.is_valid():
+            raise ValidationError
+        data = serializer.validated_data
+        user_id = data.pop('user_id')
         try:
             account = Account.objects.get(user_id=user_id)
         except ObjectDoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = InvoiceSerializer(data=request.data)
+        invoice_serializer = InvoiceSerializer(data=data)
         if serializer.is_valid():
             invoice = serializer.save()
-            Account.objects.filter(pk=account.pk).update(balance=F('balance')-invoice.total_price)
-            account.balance = account.balance-invoice.total_price
-            account.save()
-            return Response({'status': 'paid'})
+            return Response(data=InvoiceSerializer(instance=invoice).data)
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
