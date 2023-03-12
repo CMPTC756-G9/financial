7bd1f13c7487755a4d7028d9d26c468d65d29af1
Nima Modares Gorji <nima.mg@gmail.com>
2023-03-12 01:28:18 -0800

    Add default value to balance field in Account

--- account/models.py
+++ account/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Account(models.Model):
     user_id = models.IntegerField()
-    balance = models.IntegerField()
+    balance = models.IntegerField(default=1000)
 
