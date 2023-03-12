2cc5fcc2060336590bd6c108eb35349cce840714
Nima Modares Gorji <nima.mg@gmail.com>
2023-03-12 01:30:46 -0800

    Add balance deduction to invoice save method

--- invoice/models.py
+++ invoice/models.py
@@ -1,18 +1,24 @@
 from django.db import models
+from django.db.models import F
+
 from account.models import Account
 
 
 class Invoice(models.Model):
-    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='invoices')
     order_id = models.IntegerField()
 
     @property
     def total_price(self):
-        return self.items.aggregate(models.Sum('price'))
+        return self.items.aggregate(models.Sum(F('price')) * F('quantity'))
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        Account.objects.filter(pk=self.account_id).update(balance=F('balance') - self.total_price)
+        return super().save(force_insert, force_update, using, update_fields)
 
 
 class InvoiceItem(models.Model):
-    invoice_id = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='items')
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='items')
     quantity = models.IntegerField()
     product = models.CharField(max_length=100)
     price = models.IntegerField()
