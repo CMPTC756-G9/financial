95eaa214fd8ce5e369df8e891fff14b2cb85f618
Nima Modares Gorji <nima.mg@gmail.com>
2023-03-12 01:29:04 -0800

    Add balance validation to Invoice Serializer

--- invoice/serializers.py
+++ invoice/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+from account.models import Account
 from invoice.models import Invoice, InvoiceItem
 
 
@@ -15,5 +17,13 @@ class InvoiceSerializer(serializers.ModelSerializer):
         fields = ('account_id', 'order_id', 'items')
 
     items = InvoiceItemSerializer(many=True)
+
+    def validate(self, attrs):
+        items = attrs.get('items', [])
+        total_price = sum([item.get('price') * item.get('quantity') for item in items])
+        account = Account.objects.get(pk=attrs.get('account_id'))
+        if account.balance < total_price:
+            raise ValidationError
+        return attrs
 
 
